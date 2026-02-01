8d7e86bbcecfc95f4b8e444f0e71da9db6f7a072
jayInnovates <jeeaspirant39@gmail.com>
2026-02-01 21:09:16 +0000

    Add Effort Controller: direct torque control without gravity compensation

--- scripts/simple_bridge.py
+++ scripts/simple_bridge.py
@@ -9,17 +9,18 @@ class SimpleBridge(Node):
     def __init__(self):
         super().__init__('simple_bridge')
         self.env = os.environ.copy()
-        self.env['GZ_PARTITION'] = 'cobot_sim'
         
         self.create_subscription(Float64MultiArray, '/position_controller/commands', self.position_cb, 10)
         self.create_subscription(Float64MultiArray, '/velocity_controller/commands', self.velocity_cb, 10)
+        self.create_subscription(Float64MultiArray, '/effort_controller/commands', self.effort_cb, 10)
         
         self.get_logger().info('='*50)
-        self.get_logger().info('Cobot Digital Twin Bridge')
+        self.get_logger().info('Cobot Synchro 5 Digital Twin Bridge')
         self.get_logger().info('='*50)
         self.get_logger().info('Topics:')
         self.get_logger().info('  /position_controller/commands')
         self.get_logger().info('  /velocity_controller/commands')
+        self.get_logger().info('  /effort_controller/commands')
         self.get_logger().info('='*50)
         
     def send_gz(self, topic, value):
@@ -37,6 +38,12 @@ class SimpleBridge(Node):
             self.get_logger().info(f'Velocity: {[round(x,2) for x in msg.data[:6]]}')
             for i, val in enumerate(msg.data[:6]):
                 self.send_gz(f'/model/cobot/joint/joint{i+1}/cmd_vel', val)
+
+    def effort_cb(self, msg):
+        if len(msg.data) >= 6:
+            self.get_logger().info(f'Effort: {[round(x,2) for x in msg.data[:6]]}')
+            for i, val in enumerate(msg.data[:6]):
+                self.send_gz(f'/model/cobot/joint/joint{i+1}/cmd_effort', val)
 
 def main():
     rclpy.init()
